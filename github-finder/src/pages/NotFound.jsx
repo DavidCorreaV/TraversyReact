2131bdd4ce4586697fb0cad2ff85b2d5537a6424
David Correa <david.correav@gmail.com>
2022-02-10 23:51:22 -0500

    refactor to useReducer

---
+++ github-finder/src/pages/NotFound.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+const NotFound = () => {
+  return (
+    <div className="hero">
+      <div className="text-center hero-content">
+        <div className="max-w-lg">
+          <h1 className="text-8xl font-bold-mb-8">Oops!</h1>
+          <p className="text-5l mb-8 mt-4">Page not found</p>
+          <Link className="btn btn-primary btn-lg" to={"/"}>
+            {" "}
+            <FaHome className="mr-2" />
+            Back to Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
