6faeccc3a01f246c52d3c6fe347b932a442ce496
DavidCorreaV <david.correav@gmail.com>
2022-02-20 23:54:39 -0500

    Get Repo list

---
+++ github-finder/src/components/repos/RepoList.jsx
@@ -0,0 +1,12 @@
+
+
+
+const Repos = ({repos}) => {
+  return (
+    <div> 
+      <h3 className="text-3xl">Repos</h3>
+      </div>
+  )
+}
+
+export default Repos
--- github-finder/src/context/Github/GithubContext.js
+++ github-finder/src/context/Github/GithubContext.js
@@ -5,55 +5,67 @@ const GithubContext = createContext();
 const GITHUB_URL = "https://api.github.com";
 
 export const GithubProvider = ({ children }) => {
-  const initialState = {
-    user: {},
-    users: [],
-    loading: false,
-  };
-  const setLoading = () => dispatch({ type: "SET_LOADING" });
-  const [state, dispatch] = useReducer(githubReducer, initialState);
+    const initialState = {
+        user: {},
+        users: [],
+        loading: false,
+        repos: [],
+    };
+    const setLoading = () => dispatch({ type: "SET_LOADING" });
+    const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const searchUsers = async (text) => {
-    setLoading();
-    const params = new URLSearchParams({ q: text });
+    const searchUsers = async(text) => {
+        setLoading();
+        const params = new URLSearchParams({ q: text });
 
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
 
-    const { items } = await response.json();
+        const { items } = await response.json();
 
-    dispatch({ type: "GET_USERS", payload: items });
-  };
-  const getUser = async (login) => {
-    setLoading();
+        dispatch({ type: "GET_USERS", payload: items });
+    };
+    const getUser = async(login) => {
+        setLoading();
 
-    const response = await fetch(`${GITHUB_URL}/users/${login}`);
+        const response = await fetch(`${GITHUB_URL}/users/${login}`);
 
-    if (response.status === 404) {
-      window.location = "/notfound";
-    } else {
-      const data = await response.json();
+        if (response.status === 404) {
+            window.location = "/notfound";
+        } else {
+            const data = await response.json();
 
-      dispatch({ type: "GET_USER", payload: data });
-    }
-  };
+            dispatch({ type: "GET_USER", payload: data });
+        }
+    };
+    const getRepos = async(user) => {
+        setLoading();
 
-  const clearUsers = async () => {
-    dispatch({ type: "CLEAR_USERS" });
-  };
-  return (
-    <GithubContext.Provider
-      value={{
-        user: state.user,
-        users: state.users,
-        loading: state.loading,
-        getUser,
-        searchUsers,
-        clearUsers,
-      }}
-    >
-      {children}
-    </GithubContext.Provider>
-  );
+        const response = await fetch(`${GITHUB_URL}/users/${user}/repos`);
+
+
+        const repos = await response.json();
+
+        dispatch({ type: "GET_REPOS", payload: repos });
+    };
+
+    const clearUsers = async() => {
+        dispatch({ type: "CLEAR_USERS" });
+    };
+    return ( <GithubContext.Provider value = {
+            {
+                user: state.user,
+                users: state.users,
+                loading: state.loading,
+                getUser,
+                getRepos,
+                searchUsers,
+                clearUsers,
+                repos: state.repos,
+            }
+        } >
+        { children }  
+        </GithubContext.Provider>
+    );
 };
 
 export default GithubContext;
