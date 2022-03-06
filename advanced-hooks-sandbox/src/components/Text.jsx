97aed7b62d6e1de45d1515bf291d5824da2dbb16
DavidCorreaV <david.correav@gmail.com>
2022-03-06 15:18:01 -0500

    Exercise

--- advanced-hooks-sandbox/src/components/Text.jsx
+++ advanced-hooks-sandbox/src/components/Text.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Text({ text, onClick }) {
   return (
-    <div onClick={onClick}>
+    <div onClick={onClick} style={{ height: "100vh" }}>
       <p>{text[0]}</p>
       <p>- {text[1]}</p>
     </div>
