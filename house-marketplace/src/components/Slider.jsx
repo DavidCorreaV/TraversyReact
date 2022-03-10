8e28000afb935e5c7c1f70953f198b8f7c23c7d0
DavidCorreaV <david.correav@gmail.com>
2022-03-09 22:51:37 -0500

    Explore slider, and profile listings and listing deletion from frontend

---
+++ house-marketplace/src/components/Slider.jsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
+import { db } from "../firebase.config";
+
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import Spinner from "./Spinner";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/a11y";
+
+const Slider = () => {
+  const [loading, setLoading] = useState(true);
+  const [listing, setListing] = useState(null);
+  const navigation = useNavigate();
+  useEffect(() => {
+    const fetchListing = async () => {
+      const listingsRef = collection(db, "listings");
+      const q = query(listingsRef, orderBy("timestamp", "desc"), limit(5));
+      const querySnap = await getDocs(q);
+
+      let listings = [];
+      querySnap.forEach((doc) => {
+        return listings.push({ id: doc.id, data: doc.data() });
+      });
+      setListing(listings);
+      setLoading(false);
+    };
+    fetchListing();
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    listing && (
+      <>
+        <p className="exploreHeading">Recommended Places</p>
+        <Swiper
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+        >
+          {listing.map(({ data, id }) => (
+            <SwiperSlide
+              key={id}
+              onClick={() => navigation(`/category/${data.type}/${id}`)}
+            >
+              <div
+                style={{
+                  height: "300px",
+                  background: `url(${data.imgUrls[0]}}) center/cover no-repeat`,
+                }}
+                className="swiperSlideDiv"
+              >
+                <p className="swiperSlideText">{data.name}</p>
+                <p className="swiperSlidePrice">
+                  ${data.discountedPrice ?? data.regularPrice}
+                  {data.type === "rent" && " per Month"}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>{" "}
+      </>
+    )
+  );
+};
+
+export default Slider;
--- house-marketplace/src/pages/Profile.jsx
+++ house-marketplace/src/pages/Profile.jsx
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
-import { updateDoc, doc } from "firebase/firestore";
+import {
+  updateDoc,
+  doc,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  deleteDoc,
+} from "firebase/firestore";
 import { getAuth, updateProfile } from "firebase/auth";
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg";
 import homeIcon from "../assets/svg/homeIcon.svg";
+import Spinner from "../components/Spinner";
+import ListingItem from "../components/ListingItem";
 
 const Profile = () => {
   const auth = getAuth();
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -18,6 +31,26 @@ const Profile = () => {
 
   const { name, email } = formData;
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      const listingsRef = collection(db, "listings");
+      const q = query(
+        listingsRef,
+        where("userRef", "==", auth.currentUser.uid),
+        orderBy("timestamp", "desc")
+      );
+      const querySnap = await getDocs(q);
+      const listingsAcc = [];
+      querySnap.forEach((doc) => {
+        return listingsAcc.push({ id: doc.id, data: doc.data() });
+      });
+      setListings(listingsAcc);
+      setLoading(false);
+    };
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
+
   const logOut = () => {
     auth.signOut();
     navigate("/");
@@ -39,55 +72,82 @@ const Profile = () => {
   const onChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
+
+  const onDelete = async (listingId) => {
+    if (window.confirm("Are you sure?")) {
+      await deleteDoc(doc(db, "listings", listingId));
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingId
+      );
+      setListings(updatedListings);
+      toast.success("Successfully deleted");
+    }
+  };
+
+  loading && <Spinner />;
+
   return (
-    <div>
-      <div className="profile">
-        <header className="profileHeader">
-          <p className="pageHeader">My Profile</p>
-          <button className="logOut" type="button" onClick={logOut}>
-            Logout
-          </button>
-        </header>
-        <main>
-          <div className="profileDetailsHeader">
-            <p className="profileDetailsText">Personal Details</p>
-            <p
-              className="changePersonalDetails"
-              onClick={() => {
-                changeDetails && onSubmit();
-                setChangeDetails(!changeDetails);
-              }}
-            >
-              {changeDetails ? "done" : "change"}
-            </p>
-          </div>
-          <div className="profileCard">
-            <form>
-              <input
-                type="text"
-                id="name"
-                className={changeDetails ? "profileNameActive" : "profileName"}
-                disabled={!changeDetails}
-                value={name}
-                onChange={onChange}
-              />
-              <input
-                type="text"
-                id="email"
-                className={changeDetails ? "profileNameActive" : "profileName"}
-                disabled={!changeDetails}
-                value={email}
-                onChange={onChange}
-              />
-            </form>
-          </div>
-          <Link to="/create-listing" className="createListing">
-            <img src={homeIcon} alt="home" />
-            <p>Sell or Rent your home</p>
-            <img src={arrowRight} alt="arrow" />
-          </Link>
-        </main>
-      </div>
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button className="logOut" type="button" onClick={logOut}>
+          Logout
+        </button>
+      </header>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails(!changeDetails);
+            }}
+          >
+            {changeDetails ? "done" : "change"}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={changeDetails ? "profileNameActive" : "profileName"}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type="text"
+              id="email"
+              className={changeDetails ? "profileNameActive" : "profileName"}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+        <Link to="/create-listing" className="createListing">
+          <img src={homeIcon} alt="home" />
+          <p>Sell or Rent your home</p>
+          <img src={arrowRight} alt="arrow" />
+        </Link>
+        {!loading && listings?.length > 0 && (
+          <>
+            <p className="listingText">Your Listings</p>
+            <ul className="listingsList">
+              {listings.map((listing) => (
+                <ListingItem
+                  listing={listing.data}
+                  key={listing.id}
+                  idea={listing.id}
+                  onDelete={() => onDelete(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
+      </main>
     </div>
   );
 };
